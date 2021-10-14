7f4c3f72161bd3b6ff72347f13eeb732204921b3
Squigley18 <84987593+Squigley18@users.noreply.github.com>
2021-10-14 12:06:51 +0100

    Update r.wrapper.sh

--- low.sensitivity.LASTZ/r.wrapper.sh
+++ low.sensitivity.LASTZ/r.wrapper.sh
@@ -7,5 +7,5 @@
 #$ -m be
 #$ -M sq16564@essex.ac.uk
 
-./LASTZ-dotplot.R $1
+./LASTZ-dotplot.R $1 #create dotplot of hits found in LASTZ alignment; $1 is the output file from previous LASTZ alignment 
 
