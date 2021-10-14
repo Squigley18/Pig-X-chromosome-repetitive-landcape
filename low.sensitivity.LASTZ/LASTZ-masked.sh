e20478937e62bf934bde37ba83bc41d0eae85aa6
Squigley18 <84987593+Squigley18@users.noreply.github.com>
2021-10-14 12:10:08 +0100

    Update LASTZ-masked.sh

--- low.sensitivity.LASTZ/LASTZ-masked.sh
+++ low.sensitivity.LASTZ/LASTZ-masked.sh
@@ -10,5 +10,13 @@ source "/home/sq16564/Analysis-of-pig-X/shell-script-functions.sh" #pull functio
 
 
 lastz $1[94000000..95000000]  --self --nomirror --notrivial --notransition --nogapped --chain --step=100  --identity=$2  --format=rdotplot > $3
-
-
+#lastz command runs alignment on $1 given regions
+#--self performs a self alignment
+#--nomirror prevents mirror image alignments being shown
+#--notrivial prevents output of trivial self alignment block 
+#--notransition reduces the sensitivity of the alignment
+#--nogapped skips the gapped alignment stage of the LASTZ process
+#--chain chains together high scoring segment pairs to form a high scoring segment path
+#--step=100 reduces the sensitivity of the alignment
+#--identity sets the percentage identity at which to filter the hits
+#--format=dotplot creates data frame which can be used to create the naieve alignment dotplot
